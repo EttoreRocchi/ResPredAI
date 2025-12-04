605e8f6faeb07cb9aeab60a16e9e835f7c47f4b6
EttoreRocchi <ettoreroc@gmail.com>
2025-12-04 15:01:40 +0100

    [enh] increase hyperparameter grids

--- respredai/params.py
+++ respredai/params.py
@@ -9,43 +9,49 @@ PARAM_GRID = {
         },
         {
             "penalty": ["l1", "l2"],
-            "C": np.logspace(-1, 4, 6)
+            "C": np.logspace(-2, 4, 10)
         },
         {
             "penalty": ["elasticnet"],
-            "C": np.logspace(-1, 4, 6),
-            "l1_ratio": [0.5]
+            "C": np.logspace(-2, 4, 10),
+            "l1_ratio": [0.1, 0.3, 0.5, 0.7, 0.9]
         }
     ],
     "MLP": {
-        "hidden_layer_sizes": [(32, 16, 8), (32, 16), (16, 8)],
-        "activation": ['relu', 'logistic'],
+        "hidden_layer_sizes": [(64, 32, 16), (64, 32), (32, 16, 8), (32, 16), (16, 8), (64,), (32,)],
+        "activation": ['relu', 'tanh', 'logistic'],
+        "alpha": [0.0001, 0.001, 0.01, 0.1],
     },
     "XGB": {
-        "n_estimators": [25, 50, 100],
-        "learning_rate": [0.01, 0.1],
-        "max_depth": [3, 5, 7]
+        "n_estimators": [50, 100, 200],
+        "learning_rate": [0.01, 0.05, 0.1, 0.2],
+        "max_depth": [3, 5, 7, 9],
+        "subsample": [0.6, 0.8, 1.0],
+        "colsample_bytree": [0.6, 0.8, 1.0],
+        "gamma": [0, 0.1, 0.3]
     },
     "RF": {
         "n_estimators": [50, 100, 200],
-        "max_depth": [3, 5, 7, None],
-        "min_samples_split": [2, 5, 10],
-        "min_samples_leaf": [1, 2, 4]
+        "max_depth": [3, 5, 7, 10, None],
+        "min_samples_split": [2, 5, 10, 20],
+        "min_samples_leaf": [1, 2, 4, 8],
+        "max_features": ['sqrt', 'log2', None]
     },
     "CatBoost": {
         "iterations": [50, 100, 200],
         "learning_rate": [0.01, 0.05, 0.1],
-        "depth": [4, 6, 8],
-        "l2_leaf_reg": [1, 3, 5]
+        "depth": [3, 5, 7, 9],
+        "l2_leaf_reg": [1, 3, 5, 7, 9],
+        "border_count": [32, 64, 128],
+        "min_data_in_leaf": [1, 5, 10]
     },
     "TabPFN": {
     },
     "RBF_SVC": {
-        "C": np.logspace(-1, 3, 5),
-        "gamma": ['scale', 'auto'] + list(np.logspace(-3, 0, 4))
+        "C": np.logspace(-2, 4, 10),
+        "gamma": ['scale', 'auto'] + list(np.logspace(-4, 1, 8))
     },
     "Linear_SVC": {
-        "C": np.logspace(-1, 3, 5),
-        "max_iter": [5000]
+        "C": np.logspace(-2, 4, 10)
     }
 }
