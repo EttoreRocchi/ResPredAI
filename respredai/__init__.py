337b7da86c5cd4815a528b95ceb820c0f9a6aced
EttoreRocchi <ettoreroc@gmail.com>
2025-12-12 17:55:25 +0100

    [chore] update version

--- respredai/__init__.py
+++ respredai/__init__.py
@@ -4,6 +4,6 @@ ResPredAI - Antimicrobial Resistance Prediction via AI
 A machine learning pipeline for predicting antimicrobial resistance.
 """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __author__ = "Ettore Rocchi"
 __email__ = "ettore.rocchi3@unibo.it"
